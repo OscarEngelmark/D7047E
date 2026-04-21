03307a8fdf95f34ea31ea74a3153bfffabc02c84
Oscar <oscar.luen@proton.me>
2026-04-21 11:22:22 +0200

    Rename models dir

--- Lab2/src/global_config.py
+++ Lab2/src/global_config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 PROJECT_DIR = Path(__file__).resolve().parent.parent
 OUT_DIR     = PROJECT_DIR / "out"
 DATA_DIR    = PROJECT_DIR / "data"
-MODELS_DIR  = PROJECT_DIR / "Models"
+MODELS_DIR  = PROJECT_DIR / "models"
 WANDB_DIR   = PROJECT_DIR
 
 for p in [OUT_DIR, MODELS_DIR, WANDB_DIR]:
