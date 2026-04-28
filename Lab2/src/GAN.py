6c03961e5f8b24f70d1e6c26a6bdc06e616c7a37
Oscar <oscar.luen@proton.me>
2026-04-28 10:33:49 +0200

    Remove unused imports

--- Lab2/src/GAN.py
+++ Lab2/src/GAN.py
@@ -1,13 +1,7 @@
-import os
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 import numpy as np
 import wandb
 
